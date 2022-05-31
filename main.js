41a758eafdd4647ff87ebe4a9a3d7398d96c2407
catalinahb <cadehenba@gmail.com>
2022-05-30 20:06:50 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+let valor=prompt("Ingrese numero")
+let i=0
+let resultado=0
+let final=0
+while (valor>=i)
+{
+resultado=valor*i
+i=i+1
+final=final+resultado
+console.log("final")
+}
+alert("El resultado de la sumatoria de " + valor + " elevado a " + valor + " de base n=1 hasta n="+valor+ " es " + final)
